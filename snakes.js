c742cfcc6742056b447602b5afd17fb0d06b82d3
adtprsd <97732959+adtprsd@users.noreply.github.com>
2023-12-11 09:42:20 +0530

    Update snakes.js

--- snakes.js
+++ snakes.js
@@ -1,4 +1,11 @@
 function diceroll(){
-return (Math.floor(Math.random()*6)+1)
+  const dice = document.querySelector(".dice1");
+  dice.classList.add("shake");
+  setTimeout(() => {
+    dice.classList.remove("shake");
+    const.diceValue = (Math.floor(Math.random()*6)+1);
+    document.querySelector(".dice1").setAttribute("src","dice${diceValue}.png);},1000);
 }
+
+
 
