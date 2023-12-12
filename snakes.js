916736bb899a4e334f1a5e85a33a80a51de1f0e1
Aditya Narayan Mishra <153355718+SantAdityaji@users.noreply.github.com>
2023-12-12 13:59:26 +0530

    Update snakes.js

--- snakes.js
+++ snakes.js
@@ -1,8 +1,8 @@
 function diceroll(){
   const dice = document.querySelector(".dice1");
-  dice.classList.add("shake");
+  //dice.classList.add("shake");
   setTimeout(() => {
-    dice.classList.remove("shake");
+    //dice.classList.remove("shake");
     const diceValue = (Math.floor(Math.random()*6)+1);
     document.querySelector(".dice1").setAttribute("src",`dice${diceValue}.png`);},1000);
 }
