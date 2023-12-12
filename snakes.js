360e6ce0cee0615e896768551b2c69d482fe62c7
misrajiaditya <saiyangoku@gmail.com>
2023-12-12 15:05:07 +0530

    jl

--- snakes.js
+++ snakes.js
@@ -1,11 +1,31 @@
 function diceroll(){
-  const dice = document.querySelector(".dice1");
-  //dice.classList.add("shake");
-  setTimeout(() => {
-    //dice.classList.remove("shake");
-    const diceValue = (Math.floor(Math.random()*6)+1);
-    document.querySelector(".dice1").setAttribute("src",`dice${diceValue}.png`);},1000);
+  // dice.classList.remove("shake");
+  diceValue = (Math.floor(Math.random()*6)+1);
+  var myel = document.getElementById("dice");
+
+  if(diceValue == 1){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice1");
+  }
+  else if(diceValue == 2){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice2");
+  }
+  else if(diceValue == 3){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice3");
+  }
+  else if(diceValue == 4){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice4");
+  }
+  else if(diceValue == 5){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice5");
+  }
+  else if(diceValue == 6){
+      myel.classList.remove("dice1");
+      myel.classList.add("dice6");
+  }
+  // document.querySelector(".dice1").setAttribute("src",`dice${diceValue}.png`);},1000);
 }
-
-
-
